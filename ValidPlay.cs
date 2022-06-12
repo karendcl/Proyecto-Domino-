0db6d7f4429f7fc6cdea39cc62bd6c4abd4a8cb5
Francisco Vicente Suárez Bellón <95106875+FVSB@users.noreply.github.com>
2022-06-11 22:59:37 -0400

    Retirar referencias a null

--- ValidPlay.cs
+++ ValidPlay.cs
@@ -1,71 +1,103 @@
-namespace Juego
+
+namespace Juego;
+public class ClassicValidPlay : IValidPlay
 {
-    public class ClassicValidPlay : IValidPlay{
-        public bool ValidPlay(IBoard board, Token token){
-            if (board.board.Count==0) return true;
-            if (ValidPlayFront(board,token)) return true;
-            if (ValidPlayBack(board,token)) return true;
-            return false;
-        }
-
-        public bool ValidPlayFront(IBoard board, Token token){
-            return (token.Contains(board.First().Part1)) ;
-        }
-
-         public bool ValidPlayBack(IBoard board, Token token){
-            return (token.Contains(board.Last().Part2)) ;
-        }
-
-        public bool Match (int Part1, int part2){
-            return(part2.Equals(Part1));
-        }
+    public ClassicValidPlay()
+    {
+    }
+    public bool ValidPlay(IBoard board, Token token)
+    {
+        if (board.board.Count == 0) return true;
+        if (ValidPlayFront(board, token)) return true;
+        if (ValidPlayBack(board, token)) return true;
+        return false;
+    }
+    public bool FirstPlay(IBoard board)
+    {
+        if (board.board == null||board.board.Count==0) return true;
+        return false;
+    }
+    public bool ValidPlayFront(IBoard board, Token token)
+    {
+        if (FirstPlay(board)) return true;
+        return (token.Contains(board.First().Part1));
     }
 
-    public class SmallerValidPlay : IValidPlay{
-        public bool ValidPlay(IBoard board, Token token){
-            if (board.board.Count==0) return true;
-             if (ValidPlayFront(board,token)) return true;
-            if (ValidPlayBack(board,token)) return true;
-            return false;
-            
-        }
-
-         public bool ValidPlayFront(IBoard board, Token token){
-            return (token.Part1 < board.First().Part1 || 
-                    token.Part2 < board.First().Part1 ) ;
-        }
-
-         public bool ValidPlayBack(IBoard board, Token token){
-            return ( token.Part1 < board.Last().Part2 ||
-                    token.Part2 < board.Last().Part2) ;
-        }
-
-         public bool Match (int Part1, int part2){
-            return(part2 < Part1);
-        }
+    public bool ValidPlayBack(IBoard board, Token token)
+    {
+        if (FirstPlay(board)) return true;
+        return (token.Contains(board.Last().Part2));
     }
 
-     public class BiggerValidPlay : IValidPlay{
-        public bool ValidPlay(IBoard board, Token token){
-            if (board.board.Count==0) return true;
-             if (ValidPlayFront(board,token)) return true;
-            if (ValidPlayBack(board,token)) return true;
-            return false;
-            
-        }
+    public bool Match(int Part1, int part2)
+    {
+        return (part2.Equals(Part1));
+    }
+}
 
-         public bool ValidPlayFront(IBoard board, Token token){
-            return (token.Part1 > board.First().Part1 || 
-                    token.Part2 > board.First().Part1 ) ;
-        }
+public class SmallerValidPlay : IValidPlay
+{
+    public bool ValidPlay(IBoard board, Token token)
+    {
+        if (board.board.Count == 0) return true;
+        if (ValidPlayFront(board, token)) return true;
+        if (ValidPlayBack(board, token)) return true;
+        return false;
 
-         public bool ValidPlayBack(IBoard board, Token token){
-            return ( token.Part1 > board.Last().Part2 ||
-                     token.Part2 > board.Last().Part2) ;
-        }
+    }
 
-         public bool Match (int Part1, int part2){
-            return(part2 > Part1);
-        }
+    public bool ValidPlayFront(IBoard board, Token token)
+    {    if (FirstPlay(board)) return true;
+        return (token.Part1 < board.First().Part1 ||
+                token.Part2 < board.First().Part1);
+    }
+    public bool FirstPlay(IBoard board)
+    {
+        if (board.board == null||board.board.Count==0) return true;
+        return false;
+    }
+    public bool ValidPlayBack(IBoard board, Token token)
+    {     
+        if (FirstPlay(board)) return true;
+        return (token.Part1 < board.Last().Part2 ||
+                token.Part2 < board.Last().Part2);
+    }
+
+    public bool Match(int Part1, int part2)
+    {
+        return (part2 < Part1);
+    }
+}
+
+public class BiggerValidPlay : IValidPlay
+{
+    public bool ValidPlay(IBoard board, Token token)
+    {       if (FirstPlay(board)) return true;
+        if (board.board.Count == 0) return true;
+        if (ValidPlayFront(board, token)) return true;
+        if (ValidPlayBack(board, token)) return true;
+        return false;
+
+    }
+
+    public bool ValidPlayFront(IBoard board, Token token)
+    {      if (FirstPlay(board)) return true;
+        return (token.Part1 > board.First().Part1 ||
+                token.Part2 > board.First().Part1);
+    }
+   public bool FirstPlay(IBoard board)
+    {    
+        if (board.board == null||board.board.Count==0) return true;
+        return false;
+    }
+    public bool ValidPlayBack(IBoard board, Token token)
+    {    if (FirstPlay(board)) return true;
+        return (token.Part1 > board.Last().Part2 ||
+                 token.Part2 > board.Last().Part2);
+    }
+
+    public bool Match(int Part1, int part2)
+    {
+        return (part2 > Part1);
     }
 }
